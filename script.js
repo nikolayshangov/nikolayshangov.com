5717b9eade4350f6f4de6497d1d56aac19b4688c
nikolayshangov <100240526+nikolayshangov@users.noreply.github.com>
2024-07-25 13:24:50 +0300

    25-07-2024

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-// Set the date we're counting down to
-const countDownDate = new Date("Aug 24, 2024 23:59:59").getTime();
+document.addEventListener('DOMContentLoaded', () => {
+    const textElement = document.getElementById('maintenance-text');
+    let opacity = 0;
+    const fadeInInterval = 50; // interval in milliseconds
+    const fadeInDuration = 2000; // total duration in milliseconds
 
-// Update the count down every 1 second
-const countdownfunction = setInterval(function() {
-
-    // Get today's date and time
-    const now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    const distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Display the result in the element with id="timer"
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-    
-    // If the count down is finished, write some text 
-    if (distance < 0) {
-        clearInterval(countdownfunction);
-        document.getElementById("timer").innerHTML = "EXPIRED";
-    }
-}, 1000);
-
-// Set the current year in the footer
-document.getElementById("year").textContent = new Date().getFullYear();
+    const fadeIn = setInterval(() => {
+        if (opacity >= 1) {
+            clearInterval(fadeIn); // Stop the interval when fully visible
+        } else {
+            opacity += fadeInInterval / fadeInDuration;
+            textElement.style.opacity = opacity;
+        }
+    }, fadeInInterval);
+});
