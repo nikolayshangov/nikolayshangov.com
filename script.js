c5a4f9f95bec08b75455d398ba593f5c6df2bf72
Nikolay Shangov <100240526+nikolayshangov@users.noreply.github.com>
2024-08-07 00:53:21 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Add any custom JavaScript here if needed
-document.addEventListener('DOMContentLoaded', function() {
-    // Example: console log message
-    console.log("Website for Nikolay Shangov loaded successfully.");
-});
